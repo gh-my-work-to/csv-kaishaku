c510da7f1d910ec73907d5319a1855a973217ddb
gh-my-work-to <my.work.tokorozawa@gmail.com>
2015-12-14 06:22:48 +0900

    2

--- csv-to-ary.js
+++ csv-to-ary.js
@@ -9,14 +9,14 @@ function makeCsvToAry(txt)
 		for(var b = 0; b < bry.length; b++)
 		{
 			var cel = bry[b];
-			if(cel.substr(0, 1) == '"' && cel.substr(cel.length - 1, 1) != '"')
+			if(cel.substr(0, 1) == '"' && !(cel.substr(-1, 1) == '"' && !cel.match(/[^"]""$/)))
 			{
 				// またがっている
 				while(b <= bry.length - 2)
 				{
 					var cel_next = bry[++b];
 					cel += ',' + cel_next;
-					if(cel_next.substr(-1, 1) == '"')
+					if(cel_next.substr(-1, 1) == '"' && !cel_next.match(/[^"]""$/))
 					{
 						break;
 					}
